536190c306786be07a62787b2c05d9c939685987
tomasz-kwietniewski <64807094+tomasz-kwietniewski@users.noreply.github.com>
2020-06-16 12:31:31 +0200

    added javascript saving to Local Storage

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,30 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const entry = localStorage.getItem('entry');
+let result = '';
+
+if (entry) {
+    console.log(`wartość entry: ${entry}`);
+    result = entry;
+}
+
+const entryTextarea = document.querySelector('.textarea--js');
+entryTextarea.value = result;
+
+const buttonSave = document.querySelector('.save--js');
+console.log(buttonSave);
+
+buttonSave.addEventListener('click', () => {
+    localStorage.setItem('entry', entryTextarea.value);
+}
+)
+
+const buttonLoad = document.querySelector('.load--js');
+console.log(buttonLoad);
+
+buttonLoad.addEventListener('click', () => {
+    localStorage.getItem('entry', entryTextarea.value);
+}
+)
