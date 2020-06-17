ff46e884eb1fef0212db2eb8bb0fc7374ac5102b
tomasz-kwietniewski <64807094+tomasz-kwietniewski@users.noreply.github.com>
2020-06-17 11:53:30 +0200

    corrected layout and js

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,26 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
 
-const entry = localStorage.getItem('entry');
-let result = '';
+/*const entry = localStorage.getItem("entry");
+let result = "";
 
 if (entry) {
     console.log(`wartość entry: ${entry}`);
     result = entry;
-}
+}*/
 
-const entryTextarea = document.querySelector('.textarea--js');
-entryTextarea.value = result;
+const entryTextarea = document.querySelector(".textarea--js");
+/*entryTextarea.value = result;*/
 
-const buttonSave = document.querySelector('.save--js');
-console.log(buttonSave);
+const buttonSave = document.querySelector(".save--js");
+buttonSave.addEventListener("click", () => {
+  localStorage.setItem("entry", entryTextarea.value);
+});
 
-buttonSave.addEventListener('click', () => {
-    localStorage.setItem('entry', entryTextarea.value);
-}
-)
-
-const buttonLoad = document.querySelector('.load--js');
-console.log(buttonLoad);
-
-buttonLoad.addEventListener('click', () => {
-    localStorage.getItem('entry', entryTextarea.value);
-}
-)
+const buttonLoad = document.querySelector(".load--js");
+buttonLoad.addEventListener("click", () => {
+  entryTextarea.value = localStorage.getItem("entry");
+});
